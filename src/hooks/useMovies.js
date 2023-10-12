a02234f53376819ed3377d3450cd85865b3e0bb3
hardrich <richard88pquiroz@gmail.com>
2023-10-11 20:26:53 -0500

    Prevent duplicate search

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { searchMovies } from '../services/movies.js';
 
 export function useMovies({ query }) {
   const [movies, setMovies] = useState([]);
+  const previousQuery = useRef(query);
 
   const getMovies = async () => {
+    if (previousQuery.current === query) return;
+    previousQuery.current = query;
     const response = await searchMovies({ query });
     setMovies(response);
   };
